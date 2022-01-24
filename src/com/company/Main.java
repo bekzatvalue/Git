1176b5d274743da61792317ec3f9b584ad10acf5
bekzatrazhapaliuulu <bekzat.razhapaliuulu.12345@gamail.com>
2022-01-24 22:43:07 +0600

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Salam gitHub !!!");
         System.out.println("Salam gitHub !!!");
+        System.out.println("Salam gitHub !!!");
 
     }
 }
